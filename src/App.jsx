281b1c48452baa8ed8fde933570a72046247c544
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-15 21:49:23 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className="App">
-      
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import './hero.css'
+
+function Hero(){
+    return(
+        <div className="container">
+            
+        </div>
+    )
+}
+
+export default Hero
