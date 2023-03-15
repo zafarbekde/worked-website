a4d4a358cd6ac2d1eb6cfdec819dc6ec8cb59edf
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-15 22:17:45 +0500

    xcv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
 import './css/header.css'
+import HeaderLogo from '../img/logo.png'
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div className='headers'>
-            
+            <div className="head-wrap">
+                <div className="logo">
+                    <img src={HeaderLogo} />
+                </div>
+
+                <div className="menu-iteam">
+                    <ul className="iteams">
+                        <li className="iteam-link">Home</li>
+                        <li className="iteam-link">About</li>
+                        <li className="iteam-link">Service</li>
+                        <li className="iteam-link">Team</li>
+                        <li className="iteam-link">Contact</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
