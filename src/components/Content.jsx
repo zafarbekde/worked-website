aa5dcb0e1a67d5d7aaf6ae984561d53bae49efc7
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-16 20:31:48 +0500

    a

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 function Content() {
     return (
         <div className="txt">
-            <h1 className="text">Best Solution For
-                Your  Business
-                Startup</h1>
+            <div className="split">
+                <h1 className="text">Best Solution For
+                    Your  Business
+                    Startup</h1>
 
                 <span className="subtext"> Pellentesque non pretium sem. Sed sit amet diam vitae tortor mattis tincidunt et at neque.</span>
 
@@ -14,6 +15,7 @@ function Content() {
                     <a href="#">Get Started</a>
                     <a href="#" className="play-btn"></a>
                 </div>
+            </div>
         </div>
     )
 }
